a0911ac49e75b07c0d3691fa9e05581830cabe5d
Karthi <karskeyan03@gmail.com>
2020-03-26 12:34:14 +0530

    Initial Commit

---
+++ src/DemoDM.java
@@ -0,0 +1,29 @@
+public class DemoDM{
+
+    String versionController;
+    String command;
+    String commandDesc;
+
+    public void setVersionController(String versionController){
+        this.versionController = versionController;
+    }
+
+    public String getVersionController(){
+        return versionController;
+    }
+    public void setCommand(String command){
+        this.command = command;
+    }
+
+    public String getCommand(){
+        return command;
+    }
+    public void setCommandDesc(String commandDesc){
+        this.commandDesc = commandDesc;
+    }
+
+    public String getCommandDesc(){
+        return commandDesc;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[]args){
+
+        List<DemoDM> demoDMList = new ArrayList<>();
+        demoDMList = getDemoList();
+        if(demoDMList!=null && demoDMList.size() > 0){
+            for (int i = 0; i < demoDMList.size() ; i++) {
+                System.out.println("Command : "+demoDMList.get(i).getCommand() + " - Desc : "+demoDMList.get(i).getCommandDesc());
+            }
+        }
+
+    }
+
+    public static List<DemoDM> getDemoList(){
+        List<DemoDM> demoList = new ArrayList<>();
+
+        DemoDM demoDM = new DemoDM();
+        demoDM.setVersionController("Git");
+        demoDM.setCommand("git init");
+        demoDM.setCommandDesc("Initializes empty git repository");
+        demoList.add(demoDM);
+
+        demoDM = new DemoDM();
+        demoDM.setVersionController("Git");
+        demoDM.setCommand("git clone");
+        demoDM.setCommandDesc("Clones a project from the url");
+        demoList.add(demoDM);
+
+        demoDM = new DemoDM();
+        demoDM.setVersionController("Git");
+        demoDM.setCommand("git status");
+        demoDM.setCommandDesc("Checks any files that are in the working directory that are yet to staged/committed");
+        demoList.add(demoDM);
+
+        demoDM = new DemoDM();
+        demoDM.setVersionController("Git");
+        demoDM.setCommand("git log");
+        demoDM.setCommandDesc("Produces a data of all commits with date,SHA Id,commit Message");
+        demoList.add(demoDM);
+        demoDM = new DemoDM();
+        demoDM.setVersionController("Git");
+        demoDM.setCommand("git add");
+        demoDM.setCommandDesc("Move the files from working directory to staging index");
+        demoList.add(demoDM);
+
+        demoDM = new DemoDM();
+        demoDM.setVersionController("Git");
+        demoDM.setCommand("git commit");
+        demoDM.setCommandDesc("Move the files from staging index to repository");
+        demoList.add(demoDM);
+
+
+
+        return demoList;
+    }
+
+}
